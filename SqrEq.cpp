57b3c5a9a489f7c40823b2212586c57e0f338ddd
roman-lem <70330148+roman-lem@users.noreply.github.com>
2020-08-30 01:05:51 +0500

    SqrEq with unit-tests

--- SqrEq.cpp
+++ SqrEq.cpp
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
+#include <time.h>
+#include <stdlib.h>
 
 #define INF_ROOTS -1
 
+
+//-----------------------------------------------------------------------------
+
 int SquareEq(double a, double b, double c, double* x1, double* x2);
 
-int main(){
+int isZero(float number);
 
-  //const int INF_ROOTS = -1;
+//-----------------------------------------------------------------------------
+
+int Test_isZero();
+
+int Test_SqrEq();
+
+//-----------------------------------------------------------------------------
+
+int main(){
 
   double a = 0, b = 0, c = 0;
   double x1 = 0, x2 = 0;
@@ -50,9 +63,9 @@ int main(){
 
 int SquareEq(double a, double b, double c, double* x1, double* x2){
 
-  assert(std::isfinite (a));
-  assert(std::isfinite (b));
-  assert(std::isfinite (c));
+  assert(isfinite (a));
+  assert(isfinite (b));
+  assert(isfinite (c));
 
   assert(x1 != NULL);
   assert(x2 != NULL);
@@ -70,17 +83,99 @@ int SquareEq(double a, double b, double c, double* x1, double* x2){
   else{  /*if (a != 0)*/
     double d = b*b - 4*a*c;
 
-    if(d < -0.000001){
+    if(! isZero(d) && d < 0){
       return 0;
     }
-    else if (d >= -0.000001 && d <= 0.000001){
+    else if (isZero(d)){
       *x1 = -b/(2*a);
       return 1;
     }
-    else{ /*if (d > 0.000001)*/
+    else{ /*if (d > 0)*/
       *x1 = (-b + sqrt(d))/(2*a);
       *x2 = (-b - sqrt(d))/(2*a);
       return 2;
     }
   }
+  return 5;
+}
+
+//-----------------------------------------------------------------------------
+
+int Test_SqrEq(){
+
+  int a = 0,
+      b = 0,
+      c = 0;
+
+  double x1 = 0,
+      x2 = 0;
+
+  srand(time(NULL));
+
+  for(int i = 0; i < 1000; i++){
+
+    a = rand();
+    b = rand();
+    c = rand();
+
+    if(SquareEq(a, b, c, &x1, &x2) == 5){
+      printf("ERROR: a = %d, b = %d, c = %d", a, b, c);
+      return 0;
+    }
+  }
+
+  printf("Test_SquareEq PASSED");
+  return 0;
+}
+
+//-----------------------------------------------------------------------------
+
+int isZero(float number){
+
+  double accuracy = 1e-6;
+
+  return (fabs(number) <= accuracy) ? 1 : 0;
+
+}
+
+//-----------------------------------------------------------------------------
+
+int Test_isZero(){
+
+  double value = 0;
+  int expected = 0, result = 0;
+
+  #define DO_TEST   \
+  result = isZero(value); \
+  if(result == expected){  \
+    printf("Test on %d line: PASSED\n", __LINE__); \
+  } \
+  else{  \
+    printf("Test on %d line: FAILED (%d):(%d)", __LINE__, result, expected); \
+    return 0; \
+  }
+
+  value = 0;
+  expected = 1;
+  DO_TEST
+
+  value = 1;
+  expected = 0;
+  DO_TEST
+
+  value = -1;
+  expected = 0;
+  DO_TEST
+
+  value = 1e-6;
+  expected = 1;
+  DO_TEST
+
+  value = -1e-6;
+  expected = 1;
+  DO_TEST
+
+  #undef DO_TEST
+
+  return 0;
 }
